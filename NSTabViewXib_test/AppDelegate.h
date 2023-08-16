ed74adf8cbd747b4015f19ac4d2c6a86634c8e02
Gregory Casamento <greg.casamento@gmail.com>
2023-08-16 09:21:47 -0400

    Initial Commit

---
+++ NSTabViewXib_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSTabViewXib_test
+//
+//  Created by Gregory John Casamento on 8/16/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
